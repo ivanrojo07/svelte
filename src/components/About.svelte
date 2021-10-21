89accf8dbe8409a4d6713217bb96245d3a92ca8d
Ivan Rojo <ivanrojo07@gmail.com>
2021-10-21 12:54:42 -0500

    Reactividad

--- src/components/About.svelte
+++ src/components/About.svelte
@@ -1,10 +1,16 @@
 <script>
     let someText = `Backend Developer at Global Sight`;
+    let count = 0
+
+    function handleClick(){
+        count += 1;
+    }
 </script>
 <div class="About">
     <p>
         {someText}
     </p>
+    <button on:click={handleClick}>Click {count === 0 ? "" : count} </button>
 </div>
 
 <style>
